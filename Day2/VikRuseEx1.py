c7faaf2ce526249189579008c3627a366f9de17a
Viktor Seremetkoski <brutalic@gmail.com>
2017-06-21 14:54:10 -0700

    D2E9 - Reuse code, modules, virtual setup exercises

---
+++ Day2/VikRuseEx1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import re
+
+
+def main():
+    print ("Main")
+def func1():
+    print ("Function 1")
+def func2():
+    print ("Function 2")
+def func3():
+    print ("Function 3")
+
+if __name__ == "__main__":
+    main()
